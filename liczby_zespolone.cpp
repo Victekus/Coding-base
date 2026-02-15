41a2a7f8663e06ac0588e99e6d65b4447b3c82fa
Victekus <w.kasprzak@interia.pl>
2026-02-15 09:34:29 +0100

    Update liczby_zespolone.cpp

--- liczby_zespolone.cpp
+++ liczby_zespolone.cpp
@@ -25,6 +25,11 @@ public:
         // Dodajemy rzeczywiste do rzeczywistych, urojone do urojonych
         return LiczbaZespolona(this->rzeczywista + druga.rzeczywista, 
                                this->urojona + druga.urojona);
+	}
+	LiczbaZespolona operator-(const LiczbaZespolona& druga) {
+        // Dodajemy rzeczywiste do rzeczywistych, urojone do urojonych
+        return LiczbaZespolona(this->rzeczywista - druga.rzeczywista, 
+                               this->urojona - druga.urojona);	
     }
 	LiczbaZespolona operator*(const LiczbaZespolona& druga) {
     return LiczbaZespolona(
@@ -32,16 +37,32 @@ public:
         (this->rzeczywista * druga.urojona) + (this->urojona * druga.rzeczywista)  // Nowa urojona
     );
 }
-	}
 };    
 int main() {
-    LiczbaZespolona z1(3, 4);   // 3 + 4i
-    LiczbaZespolona z2(1, 2);   // 1 + 2i
+	double tab1[4];
+	for (int x=0 ; x<4; x++){
+		if (x%2==0){
+			cout<<"podajesz część rzeczywistą"<<endl;
+			cin >> tab1[x];
+		}
+		else {
+			cout<<"podajesz częśc urojoną"<<endl;
+			cin>>tab1[x];
+		}
+	}
+    LiczbaZespolona z1(tab1[0],tab1[1]);   // 3 + 4i
+    LiczbaZespolona z2(tab1[2], tab1[3]);   // 1 + 2i
     // Używamy naszego przeciążonego operatora!
     LiczbaZespolona z3 = z1 + z2; 
+	LiczbaZespolona z4 = z1 * z2;
+	LiczbaZespolona z5 = z1 - z2;
+	LiczbaZespolona z6 = z2 - z1;
 
     cout << "Liczba 1: "; z1.wyswietl(); cout << endl;
     cout << "Liczba 2: "; z2.wyswietl(); cout << endl;
     cout << "Suma (z1 + z2): "; z3.wyswietl(); cout << endl;
+	cout << "Iloczyn (z1*z2):"; z4.wyswietl(); cout << endl;
+	cout << "Różnica (z1-z2):" ; z5.wyswietl(); cout <<endl;
+	cout << "Różnica (z2-z1):" ; z6.wyswietl(); cout <<endl;
     return 0;
 }
